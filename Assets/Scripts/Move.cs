d36c5f2917a4ded0cfeeb10ace412e9fb0878746
Andrea <Andrea.Young24@ycdsbk12.ca>
2024-05-24 12:56:29 -0400

    Added coin, coin pickup and coin counter

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -1,12 +1,16 @@
+using TMPro;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 
 public class Move : MonoBehaviour
 {
     // Start is called before the first frame update
 
     public int speed = 1;
+    private int coinCounter = 0;
+    public TMP_Text counterText;
     void Start()
     {
         
@@ -22,5 +26,15 @@ public class Move : MonoBehaviour
         pos.x += h * Time.deltaTime;
         pos.y += v * Time.deltaTime;
         transform.position = pos;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Coin"))
+        {
+            collision.gameObject.SetActive(false);
+            coinCounter++;
+            counterText.text = "Coins: " + coinCounter;
+        }
     }
 }

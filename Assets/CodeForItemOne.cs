93f3fc26130641de02d277affaba6a613ba0a55b
sanianqiyao <Rachel.kuang21@gmail.com>
2024-06-03 13:37:25 -0400

    Add code
    Add code of item, shop and shop-manager. Will connect shop to event and add shop's window in next commit.

--- Assets/CodeForItemOne.cs
+++ Assets/CodeForItemOne.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CodeForItemOne : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int ItemID;
+    public Text price;
+    public Text count;
+    public GameObject ShopManager;
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        price.text = "Price: $" + ShopManager.GetComponent<ShopManagerScript>().shopItems[2, ItemID].ToString();
+        count.text = ShopManager.GetComponent<ShopManagerScript>().shopItems[3, ItemID].ToString();
+
     }
 }

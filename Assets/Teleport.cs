2a81ce78050fb878fc49c827be1ee643d221adb2
Andrea <124897594+Andreay113@users.noreply.github.com>
2024-06-10 14:21:39 -0400

    Made walls colliders and made it so enemies can not go through walls and teleported
    fixed camera error
    only player can go through teleporters

--- Assets/Teleport.cs
+++ Assets/Teleport.cs
@@ -5,15 +5,25 @@ using UnityEngine;
 public class Teleport : MonoBehaviour
 {
     private Transform player;
+    private Transform camera;
     public int vertical;
     public int horizontal;
+
+    public int verticalCamera;
+    public int horizontalCamera;
+
+    public int verticalAttack;
+    public int horizontalAttack;
+
 
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Transform>();
+
     }
 
-    
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
@@ -23,6 +33,19 @@ public class Teleport : MonoBehaviour
             pos.y += vertical;
 
             player.position = pos;
+
+            Vector3 cameraPos = camera.position;
+            cameraPos.x += horizontalCamera;
+            cameraPos.y += verticalCamera;
+            camera.position = cameraPos;
+        }
+
+        if (collision.gameObject.CompareTag("Attack"))
+        {
+            Vector2 enemyPos = collision.transform.position;
+            enemyPos.x += horizontalAttack;
+            enemyPos.y += verticalAttack;
+            collision.transform.position = enemyPos;
         }
     }
     

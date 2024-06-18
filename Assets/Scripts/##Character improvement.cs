ab484c8f6ee4fa37a356881f87ccf33834fb1fa3
Andrea <124897594+Andreay113@users.noreply.github.com>
2024-06-18 00:27:52 -0400

    Organize Code and add final touches

--- Assets/Scripts/##Character improvement.cs
+++ Assets/Scripts/##Character improvement.cs
@@ -74,35 +74,35 @@
 //		}
 //
 
-using UnityEngine;
- using System.Collections;
- using System.Collections.Generic;
+//using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
  
- [RequireComponent (typeof(Rigidbody))]
- public class PlayerController : MonoBehaviour {
+// [RequireComponent (typeof(Rigidbody))]
+// public class PlayerController : MonoBehaviour {
  
-     public float movementSpeed = 5.0f;
+//     public float movementSpeed = 5.0f;
  
-     void Start () {
+//     void Start () {
          
-     }
+//     }
      
-     void Update () {
+//     void Update () {
  
-        Rigidbody rigidbody = GetComponent<Rigidbody>();
+//        Rigidbody rigidbody = GetComponent<Rigidbody>();
  
-         if(Input.GetKey(KeyCode.W)) {
-             transform.position += Vector3.forward * Time.deltaTime * movementSpeed;
-         }
-         else if(Input.GetKey(KeyCode.S)) {
-             rigidbody.position += Vector3.back * Time.deltaTime * movementSpeed;
-         }
-         else if(Input.GetKey(KeyCode.A)) {
-             rigidbody.position += Vector3.left * Time.deltaTime * movementSpeed;
-         }
-         else if(Input.GetKey(KeyCode.D)) {
-             rigidbody.position += Vector3.right * Time.deltaTime * movementSpeed;
-         }
-     }
- }
- 	}
+//         if(Input.GetKey(KeyCode.W)) {
+//             transform.position += Vector3.forward * Time.deltaTime * movementSpeed;
+//         }
+//         else if(Input.GetKey(KeyCode.S)) {
+//             rigidbody.position += Vector3.back * Time.deltaTime * movementSpeed;
+//         }
+//         else if(Input.GetKey(KeyCode.A)) {
+//             rigidbody.position += Vector3.left * Time.deltaTime * movementSpeed;
+//         }
+//         else if(Input.GetKey(KeyCode.D)) {
+//             rigidbody.position += Vector3.right * Time.deltaTime * movementSpeed;
+//         }
+//     }
+// }
+// 	}
